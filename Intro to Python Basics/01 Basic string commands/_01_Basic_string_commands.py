e4f6bad7b244965f463a7e075fc1f5c10c0372dd
Jahedul-Hoque <Jahedul4321@gmail.com>
2025-02-24 19:43:32 +0000

    adding Basic Strings cpp

---
+++ Intro to Python Basics/01 Basic string commands/_01_Basic_string_commands.py
@@ -0,0 +1,16 @@
+loggingOn = "Shan has logged into his citrix VM."
+tradeValue = 10000
+traderName = "shan"
+
+
+Message = f"{traderName.upper()} is trading {tradeValue} GBP"
+taskEnded = loggingOn.replace ("Shan has logged into his citrix VM.", f"{traderName.capitalize()} has finished trading")
+
+
+
+print(loggingOn)
+print(Message)
+print(taskEnded)
+print()
+
+
