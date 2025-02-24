37bcc583bdb182a973569b07ecd38f65be44f2e6
Jahedul-Hoque <Jahedul4321@gmail.com>
2025-02-24 20:02:00 +0000

    adding Basic Integer .py

---
+++ Intro to Python Basics/02 Basic Integers and Floats/_02_Basic_Integers_and_Floats.py
@@ -0,0 +1,25 @@
+TradeIntegerValue = 3
+
+print(type(TradeIntegerValue)) ##prints out variable type
+
+TradeFloatValue = 3.54
+
+print(type(TradeFloatValue)) ##prints out variable type
+
+
+
+
+
+TradeValue = 1000
+
+TradeValue += 100 #adds 100 to the variable
+
+print (TradeValue)
+
+
+
+NegativeTradeValue = -1000
+
+print(abs(NegativeTradeValue)) #prints positive integer
+
+print(round(TradeFloatValue)) #prints rounded version of int
