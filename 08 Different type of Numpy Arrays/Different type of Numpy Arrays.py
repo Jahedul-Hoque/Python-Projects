d70dd7bcac8c4365c95c64daafe3e8b33b9ffa2b
jahed321 <jahedul4321@gmail.com>
2025-03-01 10:53:06 +0000

    adding to the python file

--- 08 Different type of Numpy Arrays/Different type of Numpy Arrays.py
+++ 08 Different type of Numpy Arrays/Different type of Numpy Arrays.py
@@ -24,5 +24,42 @@ f = np.array([[1,2,3]])
 RepeatArray = np.repeat(f,3, axis=0)
 print(RepeatArray, "\n")
 #repeatArray is equal to "f" array x3 times
+#axis is 0 so array should be in 3 lines
+
 RepeatArray = np.repeat(f,3, axis=1)
 print(RepeatArray, "\n")
+#axis is 1 so all values should be in the same line
+
+Array = np.ones((5, 5))
+print(Array, "\n")
+zero = np.zeros((3,3))
+print (zero, "\n")
+zero[1,1] = 9
+print (zero, "\n")
+
+Array[1:4, 1:4] = zero
+print(Array, "\n")
+
+
+a = np.array([1,2,3])
+b = a.copy()
+b[0] = 100
+print(a, "\n")
+print(b, "\n")
+#copies the values of a to b 
+#b DOESNT become a pointer to the same location as a
+
+a = a+2
+print(a, "\n")
+
+a = a*2
+print(a, "\n")
+
+a = a-2
+print(a, "\n")
+
+a = a/2
+print(a, "\n")
+
+a = a+b
+print(a, "\n")
