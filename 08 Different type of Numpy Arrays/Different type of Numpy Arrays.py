65963d9b21c57bb92a656150cea71d26ebd2dfdd
jahed321 <jahedul4321@gmail.com>
2025-03-01 11:40:44 +0000

    adding final python file

--- 08 Different type of Numpy Arrays/Different type of Numpy Arrays.py
+++ 08 Different type of Numpy Arrays/Different type of Numpy Arrays.py
@@ -1,65 +1,61 @@
 import numpy as np
 
-a= np.zeros((2,3))
-print(a, "\n")
-#matrix of 0 with 2x3
+# Creating arrays with predefined values
+a = np.zeros((2,3))
+print(a, "\n")  # Matrix of zeros (2x3)
 
 b = np.ones((4,2,3))
-print(b, "\n")
-#matrix of 99 with 4x2x3
+print(b, "\n")  # Matrix of ones (4x2x3)
 
-c= np.full((2,2,2), 99)
-print(c, "\n")
-#matrix of 99
+c = np.full((2,2,2), 99)
+print(c, "\n")  # Matrix filled with 99
 
+# Generating random arrays
 d = np.random.rand(4,2)
-print(d, "\n")
-#matrix of random numbers from 0-1 of 4x2
+print(d, "\n")  # Random values between 0-1 (4x2)
 
-e = np.random.randint(7, size =(3,3))
-print(e, "\n")
-#printing a matrix with integers 0-7 of 3x3
+e = np.random.randint(7, size=(3,3))
+print(e, "\n")  # Random integers 0-6 (3x3)
 
+# Repeating elements in an array
 f = np.array([[1,2,3]])
-RepeatArray = np.repeat(f,3, axis=0)
-print(RepeatArray, "\n")
-#repeatArray is equal to "f" array x3 times
-#axis is 0 so array should be in 3 lines
+RepeatArray = np.repeat(f, 3, axis=0)
+print(RepeatArray, "\n")  # Repeats row-wise
 
-RepeatArray = np.repeat(f,3, axis=1)
-print(RepeatArray, "\n")
-#axis is 1 so all values should be in the same line
+RepeatArray = np.repeat(f, 3, axis=1)
+print(RepeatArray, "\n")  # Repeats column-wise
 
-Array = np.ones((5, 5))
+# Creating a bordered matrix
+Array = np.ones((5,5))
 print(Array, "\n")
+
 zero = np.zeros((3,3))
-print (zero, "\n")
+print(zero, "\n")
 zero[1,1] = 9
-print (zero, "\n")
+print(zero, "\n")
 
 Array[1:4, 1:4] = zero
 print(Array, "\n")
 
-
+# Copying an array correctly
 a = np.array([1,2,3])
 b = a.copy()
 b[0] = 100
-print(a, "\n")
-print(b, "\n")
-#copies the values of a to b 
-#b DOESNT become a pointer to the same location as a
+print(a, "\n")  # a remains unchanged
+print(b, "\n")  # b is modified independently
 
-a = a+2
-print(a, "\n")
-
-a = a*2
+# Element-wise arithmetic operations
+a = a + 2
 print(a, "\n")
 
-a = a-2
+a = a * 2
 print(a, "\n")
 
-a = a/2
+a = a - 2
 print(a, "\n")
 
-a = a+b
+a = a / 2
+print(a, "\n")
+
+a = a + b
 print(a, "\n")
