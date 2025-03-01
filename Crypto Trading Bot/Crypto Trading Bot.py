75539f53f8258f42212c9e05522a8aff4ba8f297
jahed321 <jahedul4321@gmail.com>
2025-03-01 16:23:05 +0000

    adding a GetCurrentPrice function of bitcoin

--- Crypto Trading Bot/Crypto Trading Bot.py
+++ Crypto Trading Bot/Crypto Trading Bot.py
@@ -9,6 +9,25 @@ load_dotenv()
 APIKey = os.getenv("APIKey")
 Secret = os.getenv("Secret")
 client = Client(APIKey,Secret,testnet=True)
+#using APIKey and Secret paremeter which matches file values, login to the
+#                                  testing binance environment
 
 AccountBalance= client.get_account()
-print(AccountBalance)
+print(AccountBalance, "\n")
+#prints account balance of the testing account
+
+symbol = "BTCUSDT"
+#make a variable equal to the value of Bitcoin in relation to US Dollars
+
+BuyPriceThreshold = 80000
+SellPriceThreshold = 86000
+TradeQuantity = 0.001
+#set buy/sell thresholds
+#set trade quantity which we will buy/sell securities at 
+
+
+def GetCurrentPrice(symbol):
+    ticker = client.get_symbol_ticker(symbol=symbol)
+    return float(ticker["price"])
+
+print("Current price of bitcoin in USD:", GetCurrentPrice(symbol), "\n")
