b172dba24ce103ab85ec1268d14da368ae34b246
jahed321 <jahedul4321@gmail.com>
2025-03-03 21:35:24 +0000

    changing code

--- Python Basics/11 Import & Export using Pandas/Import & Export Using Pandas.py
+++ Python Basics/11 Import & Export using Pandas/Import & Export Using Pandas.py
@@ -3,7 +3,7 @@ import pandas as pd
 # Creating a DataFrame with employee information
 df1 = pd.DataFrame({
     "Employee Name": ["Jad", "Shan", "Jim", "Kevin", "Dave", "Jack", "Dan"],
-    "Title": ["EUS Engineer", "Platforms Developer", "CEO", "Platforms Developer", "KDB Developer", "Devops Engineer", "Senior EUS Engineer"],
+    "Title": ["EUS Engineer", "Platforms Developer", "CEO", "Platforms Developer", "KDB Developer", "DevOps Engineer", "Senior EUS Engineer"],
     "Salary": [40000, 150000, 1000000, 125000, 100000, 70000, 65000]
 })
 
@@ -15,19 +15,21 @@ print(pd.read_csv("My_CSV_File"), "\n")
 
 # Save DataFrame to a JSON file
 df1.to_json("My_JSON_File", index=None)
- 
+
 # Read and print JSON file
 print(pd.read_json("My_JSON_File"), "\n")
 
+# Displaying the first 5 rows
 print(df1.head(), "\n")
-# Prints the first 5 rows
 
+# Displaying the last 5 rows
 print(df1.tail(), "\n")
-# Prints the last 5 rows
 
+# Displaying 2 random rows
 print(df1.sample(2), "\n")
-# Prints 2 random rows 
 
+# Displaying statistical summary of numerical columns
 print(df1.describe(), "\n")
 
+# Printing the maximum salary
 print(df1["Salary"].max())
