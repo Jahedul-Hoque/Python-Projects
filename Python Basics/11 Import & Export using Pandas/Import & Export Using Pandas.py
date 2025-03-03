394fd31eb2e2d83e29b278e92e3aba82188a12df
jahed321 <jahedul4321@gmail.com>
2025-03-03 21:27:07 +0000

    modified python file

--- Python Basics/11 Import & Export using Pandas/Import & Export Using Pandas.py
+++ Python Basics/11 Import & Export using Pandas/Import & Export Using Pandas.py
@@ -2,19 +2,29 @@ import pandas as pd
 
 # Creating a DataFrame with employee information
 df1 = pd.DataFrame({
-    "Employee Name": ["Jad", "Shan", "Jim", "Kevin"],
-    "Title": ["EUS Engineer", "Platforms Developer", "CEO", "Platforms Developer"],
-    "Salary": [40000, 150000, 1000000, 125000]
+    "Employee Name": ["Jad", "Shan", "Jim", "Kevin", "Dave", "Jack", "Dan"],
+    "Title": ["EUS Engineer", "Platforms Developer", "CEO", "Platforms Developer", "KDB Developer", "Devops Engineer", "Senior EUS Engineer"],
+    "Salary": [40000, 150000, 1000000, 125000, 100000, 70000, 65000]
 })
 
 # Save DataFrame to a CSV file
 df1.to_csv("My_CSV_File", index=None)
 
 # Read and print CSV file
-print(pd.read_csv("My_CSV_File"))
+print(pd.read_csv("My_CSV_File"), "\n")
 
 # Save DataFrame to a JSON file
 df1.to_json("My_JSON_File", index=None)
-
+ 
 # Read and print JSON file
-print(pd.read_json("My_JSON_File"))
+print(pd.read_json("My_JSON_File"), "\n")
+
+print(df1.head(), "\n")
+# Prints the first 5 rows
+
+print(df1.tail(), "\n")
+# Prints the last 5 rows
+
+print(df1.sample(2), "\n")
+# Prints 2 random rows
+
